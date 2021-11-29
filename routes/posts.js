7612b535e4efad92971bcd1fb44855d0077a247f
Rishabhsingh715 <srishabh715@gmail.com>
2021-11-29 10:37:26 +0530

    delete comment

--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,5 @@ const passport = require('passport');
 const postsController = require('../controllers/postController');
 
 router.post('/create', passport.checkAuthentication ,postsController.create);
-
+router.get('/destroy/:id',passport.checkAuthentication,postsController.destroy);
 module.exports = router;
